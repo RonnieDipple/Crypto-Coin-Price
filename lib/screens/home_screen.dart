dca290a1028b401f4710d51f5187d282aecc7ae8
Ronnie <ronniedipple@gmail.com>
2020-04-18 21:29:48 -0500

    futureBuilder returning null

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,73 @@
+import 'package:cryptocoinprice/models/crypto_coin_model.dart';
+import 'package:cryptocoinprice/repositories/crypto_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _cryptoRepository = CryptoRepository();
+  int _page = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Top Coins'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Theme
+                  .of(context)
+                  .primaryColor,
+              Colors.grey[900],
+            ],
+          ),
+        ),
+        child: FutureBuilder(
+          future: _cryptoRepository.getTopCoins(page: _page),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(
+                  valueColor:
+                  AlwaysStoppedAnimation(Theme
+                      .of(context)
+                      .accentColor),
+                ),
+              );
+            }
+            final List<CryptoCoin> cryptoCoins = snapshot.data;
+            return ListView
+                .builder(itemCount: cryptoCoins.length,
+              itemBuilder: (BuildContext context, int index) {
+                final cryptoCoin = cryptoCoins[index];
+                return ListTile(leading: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('${++index}', style: TextStyle(
+                        color: Theme
+                            .of(context)
+                            .accentColor,
+                        fontWeight: FontWeight.w600
+                    ),
+                    ),
+                  ],
+                ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
